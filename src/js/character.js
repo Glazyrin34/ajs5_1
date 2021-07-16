8ebd0f3fa5ad6a1229d2dcf3613b11d5b51e5da2
Ilya Glazyrin <glazyrinilya2011@gmail.com>
2021-07-16 12:59:16 +0500

    task 1 added

---
+++ src/js/character.js
@@ -0,0 +1,35 @@
+export default class Character {
+    constructor(name, type, health = 100, level = 1) {
+      if (name.length < 2 || name.length > 10 || typeof type !== 'string') {
+        throw new Error('Неверные данные');
+      }
+      this.name = name;
+      this.type = type;
+      this.health = health;
+      this.level = level;
+      if (type === 'Bowerman') {
+        this.attack = 25;
+        this.defence = 25;
+      }
+      if (type === 'Daemon') {
+        this.attack = 10;
+        this.defence = 40;
+      }
+      if (type === 'Magician') {
+        this.attack = 10;
+        this.defence = 40;
+      }
+      if (type === 'Swordsman') {
+        this.attack = 40;
+        this.defence = 10;
+      }
+      if (type === 'Undead') {
+        this.attack = 25;
+        this.defence = 25;
+      }
+      if (type === 'Zombie') {
+        this.attack = 40;
+        this.defence = 10;
+      }
+    }
+}
